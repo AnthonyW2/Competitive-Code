d3ec97ec13102288ce7a64f09876790ffc19295a
Anthony Wilson <awilsonprogramming@gmail.com>
2023-12-08 14:09:37 +1100

    Added template

---
+++ aoc_2023/src/solutions/template.rs
@@ -0,0 +1,23 @@
+pub mod day_ {
+    
+    pub fn solution(lines: Vec<String>) {
+        
+        //println!("{:?}", lines);
+        
+        
+        // ==== Part 1 ==== //
+        
+        for (li, line) in (&lines).iter().enumerate() {
+            
+        }
+        
+        println!("Part 1: {}", "_");
+        
+        
+        // ==== Part 2 ==== //
+        
+        println!("Part 2: {}", "_");
+        
+    }
+    
+}
