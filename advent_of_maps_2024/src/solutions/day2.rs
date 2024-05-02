9293d3df43b2b45465958d5437951be48e09cdaf
Anthony Wilson <awilsonprogramming@gmail.com>
2024-05-02 16:09:51 +1000

    Completed day 2

--- advent_of_maps_2024/src/solutions/day2.rs
+++ advent_of_maps_2024/src/solutions/day2.rs
@@ -4,16 +4,36 @@ pub mod day2 {
         
         // ==== Part 1 ==== //
         
-        for (li, line) in (&lines).iter().enumerate() {
-            
+        let n = lines[0].parse::<u32>().unwrap();
+        
+        let mut x = 0;
+        
+        let mut res = 0;
+        
+        while x < n {
+            if sum_digits(x)%7 == 0 {
+                res += 1;
+            }
+            x += 1;
         }
         
-        println!("Part 1: {}", "_");
+        fn sum_digits(x: u32) -> u32 {
+            if x < 10 {
+                return x;
+            }
+            
+            let mut x = x;
+            let mut sum = 0;
+            
+            while x != 0 {
+                sum += x % 10;
+                x /= 10;
+            }
+            
+            return sum;
+        }
         
-        
-        // ==== Part 2 ==== //
-        
-        println!("Part 2: {}", "_");
+        println!("Part 1: {}", res);
         
     }
     
