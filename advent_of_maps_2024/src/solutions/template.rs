338eecc9233ac22fd28f57283ab9c4c7f40252eb
Anthony Wilson <awilsonprogramming@gmail.com>
2024-05-03 16:07:04 +1000

    Completed day 3 (second place!)
    Also updated code template to match the event better.

--- advent_of_maps_2024/src/solutions/template.rs
+++ advent_of_maps_2024/src/solutions/template.rs
@@ -3,17 +3,18 @@ pub mod day_ {
     pub fn solution(lines: Vec<String>) {
         
         // ==== Part 1 ==== //
+        /*
+        let n = lines[0].parse::<u32>().unwrap();
+        
+        let arr = lines[1].split_ascii_whitespace().map(|s| s.parse::<u32>().unwrap()).collect::<Vec<_>>();
         
         for (li, line) in (&lines).iter().enumerate() {
             
         }
         
+        */
+        
         println!("Part 1: {}", "_");
-        
-        
-        // ==== Part 2 ==== //
-        
-        println!("Part 2: {}", "_");
         
     }
     
