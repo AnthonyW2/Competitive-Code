4bb26fc67ce274f2481ec35a3dced6ddd316c64d
Anthony Wilson <awilsonprogramming@gmail.com>
2024-12-01 08:32:23 +1100

    Added the Advent of Code 2024 skeleton files

---
+++ aoc_2024/src/solutions/day1.rs
@@ -0,0 +1,20 @@
+pub mod day1 {
+    
+    pub fn solution(lines: Vec<String>) {
+        
+        // ==== Part 1 ==== //
+        
+        for (li, line) in (&lines).iter().enumerate() {
+            
+        }
+        
+        println!("Part 1: {}", "_");
+        
+        
+        // ==== Part 2 ==== //
+        
+        println!("Part 2: {}", "_");
+        
+    }
+    
+}
