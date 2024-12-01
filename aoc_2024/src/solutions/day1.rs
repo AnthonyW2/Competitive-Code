287e1f3c56f930a67849f7b788272fdd71eda958
Anthony Wilson <awilsonprogramming@gmail.com>
2024-12-01 17:42:11 +1100

    Completed day 1

--- aoc_2024/src/solutions/day1.rs
+++ aoc_2024/src/solutions/day1.rs
@@ -4,16 +4,39 @@ pub mod day1 {
         
         // ==== Part 1 ==== //
         
-        for (li, line) in (&lines).iter().enumerate() {
+        let mut similarity1 = 0;
+        
+        let mut l1 = Vec::new();
+        let mut l2 = Vec::new();
+        
+        for line in (&lines).iter() {
+            let split = line.split_whitespace().map(|s| s.parse::<u32>().unwrap()).collect::<Vec<_>>();
             
+            l1.push(split[0]);
+            l2.push(split[1]);
         }
         
-        println!("Part 1: {}", "_");
+        l1.sort();
+        l2.sort();
+        
+        for i in 0..(&l1).len() {
+            similarity1 += l1[i].abs_diff(l2[i]);
+        }
+        
+        println!("Part 1: {}", similarity1);
         
         
         // ==== Part 2 ==== //
         
-        println!("Part 2: {}", "_");
+        let mut similarity2 = 0;
+        
+        for i in 0..(&l1).len() {
+            let matches = l2.iter().filter(|n| **n == l1[i]).count();
+            
+            similarity2 += l1[i] * (matches as u32);
+        }
+        
+        println!("Part 2: {}", similarity2);
         
     }
     
